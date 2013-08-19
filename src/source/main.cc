93dd4191bd2fb1356272e144ddd4070a7b2ec2fb
Ben <benjamin.pabst85@gmail.com>
2013-08-19 22:56:49 +0200

    Add basic layout for the application.

--- src/source/main.cc
+++ src/source/main.cc
@@ -1,9 +1,15 @@
-#include <QApplication>
+#include <QGuiApplication>
+#include <QUrl>
 #include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+//    QApplication app(argc, argv);
+    QGuiApplication app(argc, argv);
+//    QQuickView view;
+//    view.setSource(QUrl("qrc:///qml/main.qml"));
+//    view.show();
     QQmlApplicationEngine engine(QUrl("qrc:///qml/main.qml"));
+
     return app.exec();
 }
